143034856ffc56cb2c9a281ab444ebb0fb1a0e42
AleRoncadin <74709473+AleRoncadin@users.noreply.github.com>
2023-12-20 09:10:30 +0000

    Delete Lezione12 directory

--- Lezione12/11_complex/angle.c
+++
@@ -1,7 +0,0 @@
-#include "complex.h"
-
-// Funzione per calcolare l'argomento di un numero complesso in radianti
-float angle(struct complex z)
-{
-    return atan2(z.imag, z.real);
-}
--- Lezione12/11_complex/cabs.c
+++
@@ -1,7 +0,0 @@
-#include "complex.h"
-
-// Funzione per calcolare il modulo di un numero complesso
-float cabs(struct complex z)
-{
-    return sqrt(z.real * z.real + z.imag * z.imag);
-}
