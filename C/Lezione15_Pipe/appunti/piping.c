06847070d6d79d664862e19ca1cb4d7a69fc2edb
AleRoncadin <74709473+AleRoncadin@users.noreply.github.com>
2024-01-16 21:18:27 +0100

    Update piping.c

--- C/Lezione15_Pipe/appunti/piping.c
+++ C/Lezione15_Pipe/appunti/piping.c
@@ -5,35 +5,41 @@
 
 #define MSGSIZE 14
 
-int main()
-{
-    int pipes[2] = {};
+int main() {
+    int pipes[2] = { }; // Array per i descrittori di file della pipe
 
-    if (pipe(pipes) == -1)
-    {
+    if (pipe(pipes) == -1) { // Creazione della pipe e controllo degli errori
         perror("pipe call");
         return 1;
     }
 
-    char msg[MSGSIZE] = {};
-    pid_t pid = fork();
+    char msg[MSGSIZE] = { }; // Buffer per il messaggio
 
-    switch (pid)
-    {
-    case -1:
-        perror("fork call");
-        return 2;
+    pid_t pid = fork(); // Fork per creare un processo figlio
 
-    case 0:              // processo figlio
-        close(pipes[0]); // chiude l’altro capo
-        write(pipes[1], "Hello, world!", MSGSIZE);
-        break;
+    switch (pid) {
+        case -1:
+            perror("fork call");
+            return 2;
+        case 0:
+            // Codice eseguito dal processo figlio
+            close(pipes[0]); // Chiude il lato di lettura della pipe
 
-    default:             // processo padre
-        close(pipes[1]); // chiude l’altro capo
-        read(pipes[0], msg, MSGSIZE);
-        printf("%s\n", msg);
-        wait(NULL);
+            // Scrive il messaggio nella pipe
+            write(pipes[1], "Hello, world!", MSGSIZE);
+            break;
+        default:
+            // Codice eseguito dal processo padre
+            close(pipes[1]); // Chiude il lato di scrittura della pipe
+
+            // Legge il messaggio dalla pipe
+            read(pipes[0], msg, MSGSIZE);
+
+            // Stampa il messaggio ricevuto
+            printf("%s\n", msg);
+
+            // Attendere che il processo figlio termini
+            wait(NULL);
     }
 
     return 0;
